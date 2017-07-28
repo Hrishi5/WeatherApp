75c6c4bc7c000ed1928c3f4e18c0238e07e0f9b9
Hrishi5 <hvdandroid@gmail.com>
2017-07-28 23:48:52 +0530

    start script and heroku port

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express') ;
+const fs = require('fs') ;
+const hbs = require('hbs') ;
+const port_num = process.env.PORT || 8000 ;
+var app = express() ;
+app.set('view engine','hbs') ;
+app.set('views',__dirname+'/webapp/html') ;
+app.use(function(request,response,next) {
+    var now = new Date().toString() ;
+    fs.appendFile('server.log',`${now} Request with method ${request.method}` + '\n') ;
+    next() ;
+}) ;
+
+app.get('/',function(request,response)
+        {
+    
+    response.render('index',{title:'Weather App!'}) ;
+}) ;
+
+app.listen(port_num,function()
+           {
+    console.log('Server Started successfully!') ;
+}) ;
