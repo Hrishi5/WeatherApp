64b35d5805ddbc7493bf0580fa1286c0dd858dd3
Hrishi5 <hvdandroid@gmail.com>
2017-08-01 22:52:26 +0530

    Wired REST APIs

---
+++ requestUtility.js
@@ -0,0 +1,42 @@
+const request = require('request') ;
+
+module.exports.getWeather = function(lat,long,callback) {
+    
+    request(
+    `https://api.darksky.net/forecast/e484648b80398a8f77b0afb5091a9447/${lat},${long}?units=si`,function(error,response,body)
+    {   
+            
+         callback(body) ;
+    }) ;
+    
+    
+}
+
+module.exports.getLocation = function(address,callback) {
+    if(address !== null && address !== undefined && address.trim() !== '') {
+    request(
+    'http://maps.googleapis.com/maps/api/geocode/json?address='+encodeURIComponent(address),function(error,response,body) {
+        var parsedBody = JSON.parse(body) ;
+        if(parsedBody.status.toLowerCase() === 'ok' ) {
+        callback(
+            {   successFlag:1,
+                data:{
+                    formattedAddress:parsedBody.results[0].formatted_address,
+                    latitude:parsedBody.results[0].geometry.location.lat,
+                    longitude:parsedBody.results[0].geometry.location.lng
+                     }
+            }
+        ) ;
+        }else{
+            callback({successFlag : 0,
+                      data:'Could not connect to location service!'})
+        }
+    }
+    )
+    }else{
+        callback({successFlag : 0,
+                      data:'Invalid Input!'}) ;
+    }
+}
+
+return module.exports ;
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express') ;
 const fs = require('fs') ;
 const hbs = require('hbs') ;
 const port_num = process.env.PORT || 8000 ;
+const reqUtility = require('./requestUtility.js') ;
 var app = express() ;
+var responseData = {key:'unchanged'} ;
 app.set('view engine','hbs') ;
 app.set('views',__dirname+'/webapp/html') ;
 app.use(function(request,response,next) {
@@ -16,6 +18,41 @@ app.get('/',function(request,response)
     
     response.render('index',{title:'Weather App!',para:'Testing heroku'}) ;
 }) ;
+
+app.get('/getWeatherByLatLong',function(request,response) {
+    var latitude = 18.5179431 ;
+var longitude = 73.90666329999999 ;
+        var result = reqUtility.getWeather(latitude,longitude,(result)=>
+                                           {
+            var obj = JSON.parse(result) ;
+            console.log(obj.currently.temperature) ;
+        }
+        ) ;
+        console.log(result) ;
+        }) ;
+
+app.get('/getWeather',function(request,response) {
+    reqUtility.getLocation(request.query.address,function callback(result)
+                           {
+      if(result.successFlag === 1) {  reqUtility.getWeather(result.data.latitude,result.data.longitude,function callback(weatherData) {
+          result.data.weather = JSON.parse(weatherData) ;
+          response.send(result) ;    
+        })
+                                   }
+        else{
+            response.send(result) ;
+        }
+        
+    }) ;
+    
+    
+}) ;
+
+var setData = function(data) {
+    
+    responseData = data ;
+}
+
 
 app.listen(port_num,function()
            {
